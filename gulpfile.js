21e46a0e65eff5912d96157680709a4701547c1c
David Jones <unixmonkey1@gmail.com>
2015-09-21 21:40:59 -0400

    Combine all sources to a single JS bundle

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,18 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 
-gulp.task("compile-vendor", function () {
-  return gulp.src([
-    'node_modules/angular/angular.min.js',
-    'node_modules/angular-ui-router/release/angular-ui-router.min.js'
-    ])
-    .pipe(concat("vendor.js"))
-    .pipe(gulp.dest("dist"));
-});
-
 gulp.task("compile-app", function () {
-  return gulp.src("src/**/*.js")
+  var sources = [
+    "node_modules/angular/angular.min.js",
+    "node_modules/angular-ui-router/release/angular-ui-router.min.js",
+    "src/**/*.js"
+  ]
+  return gulp.src(sources)
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("app.js"))
+    .pipe(concat("all.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["compile-vendor", "compile-app"]);
+gulp.task("default", ["compile-app"]);
