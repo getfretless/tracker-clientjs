0cfc91c24ecaf64a0494e4fe0a25bb47bc504e86
David Jones <unixmonkey1@gmail.com>
2015-09-21 21:37:12 -0400

    Compile vendor stuff to a separate file

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,22 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 
-var externalJs = [
-  'node_modules/angular/angular.min.js',
-  'node_modules/angular-ui-router/release/angular-ui-router.min.js'
-];
+gulp.task("compile-vendor", function () {
+  return gulp.src([
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js'
+    ])
+    .pipe(concat("vendor.js"))
+    .pipe(gulp.dest("dist"));
+});
 
-gulp.task("default", function () {
+gulp.task("compile-app", function () {
   return gulp.src("src/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("all.js"))
+    .pipe(concat("app.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task("default", ["compile-vendor", "compile-app"]);
