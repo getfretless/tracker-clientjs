8e9b48233b35797b085b9a1126bd1aeffb892fe2
David Jones <unixmonkey1@gmail.com>
2015-09-23 23:12:45 -0400

    Experiment with passing params and ng-click

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -1,15 +1,23 @@
 import app from '../app';
 
-export default app.directive('about', function() {
+export default app.directive('about', function($router) {
   return {
     restrict: 'EA',
     replace: true,
+    controller: function() {
+      this.goHome = function() {
+        $router.navigate('/')
+      }
+    },
+    controllerAs: 'ctrl',
     scope: {},
     template: `
       <div>
         About Us
         <a ng-link="['/Home']">Home</a>
         <a ng-link="['/About']">About</a>
+        <a href="#" ng-click="ctrl.goHome()">goHome</a>
+        <a ng-link="['/Home', { param: 'Dave'}]">Go Home Dave</a>
       </div>`
   };
 });
